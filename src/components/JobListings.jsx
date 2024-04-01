04bdf4cf83111160f66b2d22cc85c7a43208d7d0
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2024-04-01 11:36:20 +0100

    finish homepage content

---
+++ src/components/JobListings.jsx
@@ -0,0 +1,23 @@
+import jobs from "../jobs.json"
+import JobListing from "./JobListing";
+
+const JobListings = () => {
+    const recentJobs = jobs.splice(0, 3)
+    const allJobs = recentJobs.map((job) => (
+        <JobListing key={job.id} job={job}/>
+    ))
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <div className=" lg:container m-auto">
+        <h2 className="text-3xl font-bold text-blue-600 mb-6 text-center">
+          Browse Current Jobs
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            { allJobs }
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default JobListings;
